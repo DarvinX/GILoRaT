755c57ccfdd7ff7efa76d8e5f07eae8e1df25678
DarvinX <tamalhansda16@gmail.com>
2025-05-10 11:27:09 +0530

    script all exps

---
+++ run.sh
@@ -0,0 +1,4 @@
+python train.py --model AlexNet --dataset CIFAR10 --use_ortho_loss --log_name AlexNet_CIFAR10_ortho.csv
+python train.py --model AlexNet --dataset CIFAR10 --log_name AlexNet_CIFAR10.csv
+python train.py --model AlexNet --dataset MNIST --use_ortho_loss --log_name AlexNet_MNIST_ortho.csv
+python train.py --model AlexNet --dataset MNIST --log_name AlexNet_MNIST.csv
